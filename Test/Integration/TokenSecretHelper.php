12bb5d48cf5499498a199b4cc1e2f2200d890cea
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-08-27 11:35:42 +0200

    fix: [MGS-4309] adjusted tests

--- Test/Integration/TokenSecretHelper.php
+++ Test/Integration/TokenSecretHelper.php
@@ -7,6 +7,11 @@ class TokenSecretHelper
     public function prepareTokenSecretForTests()
     {
         $cacheFilePath = BP . '/var/global/lazy_resize_secret';
+
+        if (!file_exists($cacheFilePath)) { //phpcs:ignore
+            return;
+        }
+
         file_put_contents($cacheFilePath, (string)\MageSuite\LazyResize\Helper\Configuration::DEFAULT_TOKEN_SECRET); //phpcs:ignore
     }
 }
